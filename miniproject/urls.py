f79d83fc8935f6e35f536a5c54c33e4ca39c1d67
tarun <t.s.clashers@gmail.comn>
2025-08-31 16:28:06 +0530

    completed

--- miniproject/urls.py
+++ miniproject/urls.py
@@ -19,9 +19,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-
+from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tweet/', include('tweet.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
